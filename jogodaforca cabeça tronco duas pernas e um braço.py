bcd32b6eefff18d51de15921ec8f1756a7cb32bb
FelipeGCF <Felipegoichman@hotmail.com>
2015-03-23 14:56:34 -0300

    Base+Cabeça+Tronco+Duas Pernas+Um Braço

---
+++ jogodaforca cabeça tronco duas pernas e um braço.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 10:41:25 2015
+
+@author: Felipe
+"""
+
+import turtle        # Usa a biblioteca de turtle graphics
+window = turtle.Screen()    # cria uma janela
+window.bgcolor("lightblue")
+window.title("Poligonos")
+
+forca   = turtle.Turtle()  
+forca.speed(5)             
+forca.penup()      
+forca.setpos(150,-150)
+forca.pendown()
+forca.backward(250)
+forca.left(90)
+forca.forward(300)
+forca.right(90)
+forca.forward(100)
+forca.right(90)
+forca.forward(50)
+forca.right(90)
+forca.circle(20)
+forca.circle(20, 180)
+forca.right(90)
+forca.forward(100)
+forca.right(330)
+forca.forward(80)
+forca.right(180) 
+forca.forward(80)
+forca.right(225) 
+forca.forward(80)
+forca.right(180)
+forca.forward(80)
+forca.right(345)
+forca.forward(70)
+forca.right(120)
+forca.forward(60)
+
+
+
+
+

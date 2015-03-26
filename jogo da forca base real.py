86abe554297178bfaadc226828bb6b07f1e4ea7a
FelipeGCF <Felipegoichman@hotmail.com>
2015-03-26 13:45:00 -0300

    Enumerei os erros

--- jogo da forca base real.py
+++ jogo da forca base real.py
@@ -28,12 +28,11 @@ forca.forward(100)
 forca.right(90)
 forca.forward(50)
 forca.color("blue")
-
+erros=0
 while True:
    
     digitadas=[]
     acertos=0
-    erros=0
     lista = []    
     
   
@@ -51,47 +50,47 @@ while True:
              print("Voce acertou!")
          
     
-    
-    erros+=1
-    print("Voce errou!")
-    if erros==1:       
-       forca.right(90)
-       forca.circle(20)
+    else:
+        erros+=1
+        print("Voce errou!")
+        if erros==1:       
+           forca.right(90)
+           forca.circle(20)
          
-    if erros==2:
-       forca.circle(20, 180)
-       forca.right(90)
-       forca.forward(100)
+        if erros==2:
+           forca.circle(20, 180)
+           forca.right(90)
+           forca.forward(100)
            
                     
-    if erros==3:
-       forca.right(330)
-       forca.forward(80)
-       forca.right(180) 
-       forca.forward(80)
+        if erros==3:
+           forca.right(330)
+           forca.forward(80)
+           forca.right(180) 
+           forca.forward(80)
+    
+                    
+        if erros==4:
+           forca.right(225) 
+           forca.forward(80)
+           forca.right(180)
+           forca.forward(80)
         
                     
-    if erros==4:
-       forca.right(225) 
-       forca.forward(80)
-       forca.right(180)
-       forca.forward(80)
-        
-                    
-    if erros==5:
-       forca.right(345)
-       forca.forward(70)
-       forca.right(120)
-       forca.forward(60)
+        if erros==5:
+           forca.right(345)
+           forca.forward(70)
+           forca.right(120)
+           forca.forward(60)
      
                     
-    if erros==6:
-       forca.right(180)
-       forca.forward(60)
-       forca.right(300)
-       forca.forward(60)
-       print("Enforcado!")
-       break
+        if erros==6:
+           forca.right(180)
+           forca.forward(60)
+           forca.right(300)
+           forca.forward(60)
+           print("Enforcado!")
+           break
     
     
     if digitadas in leitura :

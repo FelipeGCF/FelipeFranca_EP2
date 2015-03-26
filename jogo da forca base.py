21b2bd281b38335743280937790c6b33068ec1a6
FelipeGCF <Felipegoichman@hotmail.com>
2015-03-26 08:52:43 -0300

    Escolhe uma palavra do arquivo

--- jogo da forca base.py
+++ jogo da forca base.py
@@ -1,5 +1,17 @@
-import turtle        # Usa a biblioteca de turtle graphics
-window = turtle.Screen()    # cria uma janela
+
+
+import turtle 
+import random
+listalimpa = []
+arquivo=open("entrada.txt",encoding="utf-8")
+leitura=arquivo.readlines()
+print(leitura)
+for i in range(len(leitura)):
+    listalimpa.append(leitura[i].strip())
+    print(listalimpa)
+    z= random.choice(listalimpa)
+    print(z)
+window = turtle.Screen()    
 window.bgcolor("lightblue")
 window.title("Poligonos")
 
@@ -17,61 +29,77 @@ forca.right(90)
 forca.forward(50)
 forca.color("blue")
 
-palavra=input("Digite a palavra secreta").lower().strip()
-for x in range(100):
-    print()
-digitadas=[]
-acertos=[]
-erros=0
 while True:
-    senha=""
-    for letra in palavra:
-        senha+=letra if letra in acertos else "."
-        print(senha)
-        if senha==palavra:
-            print("Prabéns, voce ganhou o jogo!")
-            break
-        tentativa=input("\nDigite uma letra:").lower().strip()
-        if tentativa in digitadas:
-            print("Voce ja tentou esta letra!")
-            continue
-        else:
-            digitadas+=tentativa
-            if tentativa in palavra:
-                acertos+=tentativa
-            else:
-                erros+=1
-                print("Voce errou!")
-                if erros==1:       
-                    forca.right(90)
-                    forca.circle(20)
-                    forca.color("blue")
-                if erros==2:
-                    forca.circle(20, 180)
-                    forca.right(90)
-                    forca.forward(100)
-                if erros==3:
-                    forca.right(330)
-                    forca.forward(80)
-                    forca.right(180) 
-                    forca.forward(80)
-                if erros==4:
-                    forca.right(225) 
-                    forca.forward(80)
-                    forca.right(180)
-                    forca.forward(80)
-                if erros==5:
-                    forca.right(345)
-                    forca.forward(70)
-                    forca.right(120)
-                    forca.forward(60)
-                if erros==6:
-                    forca.right(180)
-                    forca.forward(60)
-                    forca.right(300)
-                    forca.forward(60)
-                    print("Enforcado!")
-                    break
+   
+    digitadas=[]
+    acertos=0
+    erros=0
+    lista = []    
+    
+  
+    
+    window = turtle.write
+    senha=''
+    
+    tentativa=input("\nDigite uma letra:").lower().strip()
+    if digitadas in leitura:
+       print("Voce acertou!")
+       digitadas += senha
+       continue
+       while True:
+             acertos+=1
+             print("Voce acertou!")
+         
+    
+    else:
+        erros+=1
+        print("Voce errou!")
+        if erros==1:       
+           forca.right(90)
+           forca.circle(20)
+         
+           if erros==2:
+              forca.circle(20, 180)
+              forca.right(90)
+              forca.forward(100)
+           
+                    
+              if erros==3:
+                  forca.right(330)
+                  forca.forward(80)
+                  forca.right(180) 
+                  forca.forward(80)
+        
+                    
+                  if erros==4:
+                      forca.right(225) 
+                      forca.forward(80)
+                      forca.right(180)
+                      forca.forward(80)
+        
+                    
+                      if erros==5:
+                         forca.right(345)
+                         forca.forward(70)
+                         forca.right(120)
+                         forca.forward(60)
+     
+                    
+                         if erros==6:
+                           forca.right(180)
+                           forca.forward(60)
+                           forca.right(300)
+                           forca.forward(60)
+                           print("Enforcado!")
+                           break
+    
+    
+    if digitadas in leitura :
+       print("Voce acertou")
+       
+          
+       
+                
 
 
 
